f4edb800c77952ed1767994fa8689ef1f6ba6631
SSX <mr.dimov77@gmail.com>
2023-10-19 20:33:19 +0300

    Changces in home controller for the test

--- web/src/main/java/ssx/info/web/controller/HomeController.java
+++ web/src/main/java/ssx/info/web/controller/HomeController.java
@@ -3,35 +3,22 @@ package ssx.info.web.controller;
 import org.springframework.stereotype.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 
 @Controller
 public class HomeController {
-    @GetMapping("/")
-    public String greeting() {
-        return "index";
-    }
-    @GetMapping("/login")
-    public String login() {
-        return "login";
-    }
-    @GetMapping("/admin")
-    public String admin() {
-        return "admin";
+    @ResponseBody
+    @RequestMapping(value = "/",method = RequestMethod.GET)
+    public String index(){
+        return "hello world";
     }
 
-    @GetMapping("/manager")
-    public String manager() {
-        return "manager";
-    }
-
-    @GetMapping("/employee")
-    public String employee() {
-        return "employee";
-    }
-
-    @GetMapping("/logout")
-    public String logout() {
-        return "redirect:/login";
+    @ResponseBody
+    @RequestMapping(value = "/about",method = RequestMethod.GET)
+    public String about(){
+        return "about";
     }
 }
