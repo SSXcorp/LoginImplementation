98b1ea08fbe7f2134e1df33d8924d48f28d20add
SSX <mr.dimov77@gmail.com>
2023-10-19 20:36:34 +0300

    Changes in User data file + SecurityConfig file

--- web/src/main/java/ssx/info/web/config/SecurityConfig.java
+++ web/src/main/java/ssx/info/web/config/SecurityConfig.java
@@ -1,54 +1,32 @@
 package ssx.info.web.config;
 
-import org.springframework.boot.autoconfigure.security.reactive.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-import static org.springframework.security.config.Customizer.withDefaults;
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
-    public static PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
-    }
-    @Bean
-    public SecurityFilterChain filterSecurity(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-                .authorizeHttpRequests((authorize) ->
-                        authorize.anyRequest().authenticated()
-                                .requestMatchers("/User-profile").permitAll()
-                ).formLogin(
-                        form -> form
-                                .loginPage("/login")
-                                .loginProcessingUrl("/login")
-                                .defaultSuccessUrl("/")
-                                .permitAll()
-                ).logout(
-                        logout -> logout
-                                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                                .permitAll()
-                );
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests()
+                .anyRequest().authenticated();
+        http
+                .formLogin();
+
         return http.build();
     }
-}
 
-//    @Bean
-//    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        return http
-//                .authorizeHttpRequests(auth -> {
-//                    auth.requestMatchers("/", "/home","/home.css").permitAll();
-//                    auth.anyRequest().authenticated();
-//                })
-//                .oauth2Login(withDefaults())
-//                .formLogin(withDefaults())
-//                .build();
-//    }
+    @SuppressWarnings("deprecation")
+    @Bean
+    public NoOpPasswordEncoder passwordEncoder() {
+        return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
+    }
+
+}
 
--- web/src/main/java/ssx/info/web/model/User.java
+++ web/src/main/java/ssx/info/web/model/User.java
@@ -1,64 +1,40 @@
 package ssx.info.web.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-import jakarta.persistence.*;
-import org.hibernate.annotations.LazyCollection;
-import org.hibernate.annotations.LazyCollectionOption;
-
-import java.util.Set;
-
-@Entity
-@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
 
-    private String name;
-    @Column(nullable = false, unique = true)
-    private String username;
-    @Column(nullable = false, unique = true)
+    private long id;
+
     private String email;
-    @Column(nullable = false)
-    private String password;
-    @ManyToMany
-    @LazyCollection(LazyCollectionOption.FALSE)
-    private Set<Role> roles;
 
-    public Integer getId() {
-        return id;
-    }
-    public void setId(Integer id) {
-        this.id = id;
-    }
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public String getUsername() {
-        return username;
-    }
-    public void setUsername(String username) {
-        this.username = username;
-    }
-    public String getEmail() {
-        return email;
-    }
-    public void setEmail(String email) {
+    private String password;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String profilePhotoUrl;
+
+    private String country;
+
+    private String occupation;
+
+    private Date dateOfBirth;
+
+    private String gender;
+
+    private String role;
+
+    public User(String email, String password, String firstName, String lastName) {
         this.email = email;
-    }
-    public String getPassword() {
-        return password;
-    }
-    public void setPassword(String password) {
         this.password = password;
-    }
-    public Set<Role> getRoles() {
-        return roles;
-    }
-    public void setRoles(Set<Role> roles) {
-        this.roles = roles;
+        this.firstName = firstName;
+        this.lastName = lastName;
     }
 }
