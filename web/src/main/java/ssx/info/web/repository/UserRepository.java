bd3b0d9e42c829ed11316d2175f48f78565b6009
SSX <mr.dimov77@gmail.com>
2023-10-19 20:37:54 +0300

    Adding repository test file

--- web/src/main/java/ssx/info/web/repository/UserRepository.java
+++ web/src/main/java/ssx/info/web/repository/UserRepository.java
@@ -1,15 +1,11 @@
 package ssx.info.web.repository;
 
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ssx.info.web.model.User;
 
-import java.util.Optional;
-
 @Repository
-public interface UserRepository extends JpaRepository<User, Integer> {
-    Optional<User> findByUsernameOrEmail(String username, String email);
+public class UserRepository {
+    public User findUserByEmail(String email){
+        return new User(email,"123456","Farhan","Tanvir");
+    }
 }
