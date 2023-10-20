8ee6252ad92f46b8051be981a680f7d12afee658
SSX <mr.dimov77@gmail.com>
2023-10-20 18:45:43 +0300

    Changing in UserRepository file

--- web/src/main/java/ssx/info/web/repository/UserRepository.java
+++ web/src/main/java/ssx/info/web/repository/UserRepository.java
@@ -1,11 +1,15 @@
 package ssx.info.web.repository;
 
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ssx.info.web.model.User;
 
 @Repository
-public class UserRepository {
-    public User findUserByEmail(String email){
-        return new User(email,"123456","Farhan","Tanvir");
-    }
+public interface UserRepository extends JpaRepository<User,Long> {
+
+    @Query("SELECT u FROM User u WHERE u.email = :email")
+    User findUserByEmail(@Param("email") String email);
 }
