8dc5de07a49ec7c71cdb0e0a2331ec8891504324
SSX <mr.dimov77@gmail.com>
2023-10-21 20:43:15 +0300

    Editing security config for permission to the home page

--- web/src/main/java/ssx/info/web/config/SecurityConfig.java
+++ web/src/main/java/ssx/info/web/config/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((authorize) -> authorize
-                        .requestMatchers("/login**","/logout**", "/register").permitAll()
+                        .requestMatchers("/login**","/logout**", "/register", "/").permitAll()
                         .anyRequest().authenticated()
                 );
         http
